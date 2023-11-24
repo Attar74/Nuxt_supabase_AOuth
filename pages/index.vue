e53370b2bb1f08a617a2ef1fde0770df2bf0a80e
Attar74 <m.elattar.dev@gmail.com>
2023-11-24 23:04:19 +0200

    fix mixing routes

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <section class="bg-gray-50">
     <div
       class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
+			v-if="loggedInUser"
     >
       <div class="my-auto">
         <div>
